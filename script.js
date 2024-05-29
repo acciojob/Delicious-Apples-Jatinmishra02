057d872839156796551f0d68780e4cf98d179827
Jatin Mishra <104919571+Jatinmishra02@users.noreply.github.com>
2024-05-30 01:45:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+function changePhoto(src) {
+    document.getElementById('main-photo').src = src;
+}
+
+function changeMainPhoto(src) {
+    event.preventDefault();
+    document.getElementById('main-photo').src = src;
+}
